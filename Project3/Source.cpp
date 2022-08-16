228a62b78ffdc2dd5533ca9d58504dbebbd6d9b6
ADVICE_003 <ADVICE_003@LAPTOP-37GJTC27>
2022-08-16 09:28:22 +0700

    Add project files.

---
+++ Project3/Source.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<iostream>
+int a, b, c;
+
+int main() {
+    scanf_s("%d %d %d", &a, &b, &c);
+    int sum = a + b + c;
+    if (sum <= 100 && sum >= 80) {
+        printf("A");
+    }
+    else if (sum < 80 && sum >= 75) {
+        printf("B+");
+    }
+    else if (sum < 75 && sum >= 70) {
+        printf("B");
+    }
+    else if (sum < 70 && sum >= 65) {
+        printf("C+");
+    }
+    else if (sum < 65 && sum >= 60) {
+        printf("C");
+    }
+    else if (sum < 60 && sum >= 55) {
+        printf("D+");
+    }
+    else if (sum < 55 && sum >= 50) {
+        printf("D");
+    }
+    else if (sum < 50 && sum >= 0) {
+        printf("F");
+    }
+}
